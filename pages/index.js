766bc250c108894472bf0cc303a6444d0a3ba2e7
Alexander Kalaj <adkalaj@gmail.com>
2023-01-01 18:06:31 +0000

    functional login

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
       <div className="text-center">
         <p className="text-3xl">Welcome to the Home Page</p>
         <p>Please take off your shoes.</p>
-        <p className="text-xs italic">Thank you.</p>
+        <p className="text-xs italic">Thank you kindly.</p>
       </div>
     </>
   );
--- pages/login.js
+++ pages/login.js
@@ -5,14 +5,20 @@ const login = () => {
   const { data: session } = useSession();
   const buttonClass =
     "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded";
+  const imgClass = "py-2"
 
   if (session) {
     return (
       <div>
-        <p>Welcome, {session.user.email}</p>
+        <p>Welcome, {session.user.name}</p>
+        <p>Your Email: {session.user.email}</p>
+        <p>Your Profile Pic:</p>
+        <img className={imgClass} src={session.user.image}  alt="User Logo" />
+        <div className="py-2">
         <button className={buttonClass} onClick={() => signOut()}>
           Sign Out
         </button>
+        </div>
       </div>
     );
   } else {
